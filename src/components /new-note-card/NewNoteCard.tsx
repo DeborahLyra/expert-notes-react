5771d68a9bbdf6f54594aef7c60a1d0657e2d08d
Deborah Lyra <deborahp.lyra@gmail.com>
2024-02-07 10:15:20 -0300

    Iniciando projeto

---
+++ src/components /new-note-card/NewNoteCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NewNoteCard() {
+    return (
+
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+            <span className="text-sm font-medium text-slate-200">
+                Adicionar nota
+            </span>
+            <p className="text-sm leading-6 text-slate-400">
+                Grave um texto em áudio que será convertido automaticamente
+            </p>
+        </div>
+
+    )
+}
+
+export default NewNoteCard
